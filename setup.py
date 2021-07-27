f4a1b3c1756a0d34d8ec92cfb47f1971f655e44c
Anton Shemyakov <shemyanton@gmail.com>
2021-07-27 16:53:00 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fast_image_inspection',
+      version='0.0.1',
+      description='A library for imaging dataset exploration',
+      url='https://github.com/anton-ai4m/FastImageInspection',
+      author='Anton Shemyakov',
+      author_email='anton@ai4medicine.com',
+      license='MIT',
+      packages=['fast_image_inspection'],
+      zip_safe=False)
