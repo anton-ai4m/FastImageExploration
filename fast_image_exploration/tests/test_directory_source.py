388c326804c7c278be7daee1202d59e09b3f3582
Anton Shemyakov <shemyanton@gmail.com>
2021-07-29 13:04:01 +0000

    Fixed relative path issue in tests

--- fast_image_exploration/tests/test_directory_source.py
+++ fast_image_exploration/tests/test_directory_source.py
@@ -1,14 +1,18 @@
+import os
+from pathlib import Path
 from unittest import TestCase
 from fast_image_exploration import DirectoryDataSource
 
 class TestDirectoryDataSource(TestCase):
 
     def test_directory_construction(self):
+        test_path = Path(__file__).parent
         directory = 'mnist/mnist_png/'
-        source = DirectoryDataSource(directory)
+        source = DirectoryDataSource(os.path.join(test_path, directory))
         self.assertEqual(len(source.list_images()), 20)
 
     def test_file_construction(self):
+        test_path = Path(__file__).parent
         file = 'mnist/data.txt'
-        source = DirectoryDataSource(file)
+        source = DirectoryDataSource(os.path.join(test_path, file))
         self.assertEqual(len(source.list_images()), 20)
