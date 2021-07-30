6fc3668df848ac284369a9c7b83995b9d6fa78fb
Anton Shemyakov <shemyanton@gmail.com>
2021-07-30 12:26:28 +0000

    Removed hello world

--- fast_image_exploration/__init__.py
+++ fast_image_exploration/__init__.py
@@ -1,3 +1,2 @@
-from .main import main
 from .dataset_explorer import DatasetExplorer
 from .source.directory_source import DirectoryDataSource
--- fast_image_exploration/tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import fast_image_exploration
-
-class TestMain(TestCase):
-    def test_correct_output(self):
-        output = fast_image_exploration.main()
-        self.assertEquals(output, "Hello world")
