6cec347142d2674503d77e81abe528a27bc6abf5
Anton Shemyakov <shemyanton@gmail.com>
2021-08-03 15:58:32 +0000

    Added test segmentation dataset

--- fast_image_exploration/tests/test_directory_source.py
+++ fast_image_exploration/tests/test_directory_source.py
@@ -5,22 +5,42 @@ from fast_image_exploration import DirectoryDataSource
 
 class TestDirectoryDataSource(TestCase):
 
-    def test_file_construction(self):
+    def test_mnist_file_construction(self):
         test_path = Path(__file__).parent
         file = 'mnist/data.txt'
         source = DirectoryDataSource(os.path.join(test_path, file))
         self.assertEqual(len(source.list_images()), 20)
 
-    def test_image_load(self):
+    def test_mnist_image_load(self):
         test_path = Path(__file__).parent
         file = 'mnist/data.txt'
         source = DirectoryDataSource(os.path.join(test_path, file))
         shape = source.get_image(source.list_images()[0]).shape
         self.assertEqual(shape, (28, 28))
 
-    def test_label_load(self):
+    def test_mnist_label_load(self):
         test_path = Path(__file__).parent
         file = 'mnist/data.txt'
         source = DirectoryDataSource(os.path.join(test_path, file))
         label = source.get_label(source.list_images()[8])
         self.assertEqual(label, '4')
+
+    def test_segmentation_file_construction(self):
+        test_path = Path(__file__).parent
+        file = 'segmentation/data.txt'
+        source = DirectoryDataSource(os.path.join(test_path, file))
+        self.assertEqual(len(source.list_images()), 10)
+
+    def test_segmentation_image_load(self):
+        test_path = Path(__file__).parent
+        file = 'segmentation/data.txt'
+        source = DirectoryDataSource(os.path.join(test_path, file))
+        shape = source.get_image(source.list_images()[0]).shape
+        self.assertEqual(shape, (256, 256, 4))
+
+    def test_segmentation_label_load(self):
+        test_path = Path(__file__).parent
+        file = 'segmentation/data.txt'
+        source = DirectoryDataSource(os.path.join(test_path, file))
+        label = source.get_label(source.list_images()[8])
+        self.assertEqual(label, 'labels/381.png')
